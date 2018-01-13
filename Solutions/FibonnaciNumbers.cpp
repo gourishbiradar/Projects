ad3f718141031b61c3cf098a9d8b6d4defd00d83
gourishbiradar <biradar.gourish@gmail.com>
2018-01-14 00:18:56 +0530

    Added Solution to Nth Fibonnaci number calculator

---
+++ Solutions/FibonnaciNumbers.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+void multiply(long long int F[2][2], long long int M[2][2])
+{
+  long long int x =  F[0][0]*M[0][0] + F[0][1]*M[1][0];
+  long long int y =  F[0][0]*M[0][1] + F[0][1]*M[1][1];
+  long long int z =  F[1][0]*M[0][0] + F[1][1]*M[1][0];
+  long long int w =  F[1][0]*M[0][1] + F[1][1]*M[1][1];
+ 
+  F[0][0] = x;
+  F[0][1] = y;
+  F[1][0] = z;
+  F[1][1] = w;
+}
+void power(long long int F[2][2], int n)
+{
+	if( n == 0 || n == 1)
+      return;
+  long long int M[2][2] = {{1,1},{1,0}};
+ 
+  power(F, n/2);
+  multiply(F, F);
+ 
+  if (n%2 != 0)
+     multiply(F, M);
+}
+int FibonnaciMatrix(int n)
+{
+  long long int F[2][2] = {{1,1},{1,0}};
+  if (n == 0)
+      return 0;
+  power(F, n-1);
+  return F[0][0];
+}
+int main(int argc, char** argv)
+{
+    if(argc<1)
+    {
+        cout<<"Please enter the term who's fibonnaci value you want to know";
+        system("pause");
+        return -1;
+    }
+    cout<<"Fibonnaci "<<atoi(argv[1])<<" is "<<FibonnaciMatrix(atoi(argv[1]))<<endl;
+    system("pause");
+    return 0;
+}
