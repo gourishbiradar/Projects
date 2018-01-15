98e1d8f8d5d44a65c6bc1cc271d3015771a6d9ed
gourishbiradar <biradar.gourish@gmail.com>
2018-01-15 10:43:42 +0530

    Made changes to limit of prime numbers

--- Solutions/Sieve.cpp
+++ Solutions/Sieve.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <limits> 
 using namespace std;
-
-#define MAXN INT_MAX
-bool primes[MAXN];
+bool primes[INT_FAST16_MAX];
 void sieve(int n)
 {
     int i;
     if(n<2)
     return ; // no primes smaller than 2
-    for(i=2;i<MAXN;i++)
+    for(i=2;i<INT_FAST16_MAX;i++)
         primes[i]=true;
-    for(i=4;i<MAXN;i+=2)
+    for(i=4;i<INT_FAST16_MAX;i+=2)
     primes[i]=false;
-    for(i=3;i*i<MAXN;i+=2)
+    for(i=3;i*i<INT_FAST16_MAX;i+=2)
     {
         if(primes[i])
         {
-            for(int j=i;j<MAXN;j+=i)
+            for(int j=i;j<INT_FAST16_MAX;j+=i)
                 primes[j]=false;
             primes[i]=true;
         }
@@ -29,8 +27,8 @@ int main()
     int n;
     cout<<"Enter a number upto which primes are to be calculated:";
     cin>>n;
-    if(n>MAXN)
-        n=MAXN;
+    if(n>INT_FAST16_MAX)
+        n=INT_FAST16_MAX;
     sieve(n);
     cout<<"Primes lower than "<<n<<" are :";
     for(int i=2;i<=n;i++)
