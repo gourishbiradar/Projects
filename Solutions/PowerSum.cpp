cb54373ad5ca477a3dc1b76bef711966fee837ac
gourishbiradar <biradar.gourish@gmail.com>
2018-01-20 18:34:53 +0530

    Added nth root approach

--- Solutions/PowerSum.cpp
+++ Solutions/PowerSum.cpp
@@ -1,6 +1,5 @@
 //Program to find the number of all possible ways of writing x = sum of (number raised to N) 
 //Programming paradigm - backtracking
-
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -21,10 +20,37 @@ double SubSet(double x, double n, double current_num)
         return 0;
     }
 }
+double nthroot(double x, int n, double precision=0.0001)
+{
+    if(!x)
+        return 0;
+    double x1 = x;
+    double x2 = x/n; //first division
+    while(abs(x1-x2)>precision)
+    {
+        x1=x2;
+        x2 = ((n-1.0)*x2 + x/pow(x2,n-1.0))/n;
+    }
+    return x2;
+}
+double NSums(double x, double n,double highest_num)
+{
+    if(highest_num<0)
+        return 0;
+    if(x==0)
+        return 1;
+    if(x<0)
+        return 0;
+    return NSums(x-pow(highest_num,n),n,highest_num-1)+NSums(x,n,highest_num-1);
+}
 int main()
 {
     double x,n;
     cin>>x>>n;
-    cout<<SubSet(x,n,1); // start with 1, check if you can take it as part of one of the solutions or not, continue with the next number and so on
+    //Subset sum approach
+    //cout<<SubSet(x,n,1); // start with 1, check if you can take it as part of one of the solutions or not, continue with the next number and so on
+    //nth root appraoch
+    double highest_num = nthroot(x,n);
+    cout<<NSums(x,n,highest_num);
     return 0;
 }
